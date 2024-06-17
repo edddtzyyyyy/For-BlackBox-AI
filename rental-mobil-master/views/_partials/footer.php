0096c3ccf5b8743884af7e33a1ac38e535b5816d
Sayid Abrar Fadhlurrahman <161542855+edddtzyyyyy@users.noreply.github.com>
2024-06-18 01:42:12 +0700

    Add files via upload
    FOR BLACKBOX AI

---
+++ rental-mobil-master/views/_partials/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>
+                Sayid Abrar Fadhlurrahman &copy; Aplikasi Rental Mobil <?= date('Y') ?>
+            </span>
+        </div>
+    </div>
+</footer>
